4c3862c5f5a141aa1e82f6e7e9bc1b5cfe5111a6
joselesquinas <tecnicjoncs@gmail.com>
2022-02-16 12:41:57 +0100

    ejercicio 4

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -8,10 +8,22 @@ import { Component, OnInit, Input } from '@angular/core';
 
 export class EscenaComponent implements OnInit {
    // titulo01 = "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial";
+    
+   public currentSentence: number = 0;
+   @Input() titulo: any; // decorate the property with @Input()
 
-   @Input() titulo:string = ''; // decorate the property with @Input()
+   prevTitulo(){
+      this.currentSentence -= 1;
+   }
+   
+   nextTitulo(){
+      this.currentSentence += 1;
+   }
 
-   constructor() { }
+   setUserAnswer(num:number) { }
+
+   constructor() {
+    }
 
    ngOnInit(): void {
    }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+   selector: 'app-home',
+   templateUrl: './home.component.html',
+   styleUrls: ['./home.component.sass']
 })
 
 export class HomeComponent implements OnInit {
 
    titulos = [
-      "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial", 
-      "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.", 
-      "L'heroi va decidir travessar la porta que el portava a casa", 
-      "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..."
-    ];
-    currentItem = "";
-    
-    
+      {
+         txt: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
+         img: '1.jpg',
+      },
+      {
+         txt: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
+         img: '2.jpg',
+      },
+      {
+         txt: "L'heroi va decidir travessar la porta que el portava a casa",
+         img: '3.jpg',
+      },
+      {
+         txt: 'Mentrestant, altres herois no van tenir tanta sort en la seva elecció ...',
+         img: '4.jpg',
+      },
+   ];
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+   constructor() { }
+
+   ngOnInit(): void {
+   }
 
 }
